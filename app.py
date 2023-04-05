8aad173b75b3d9233864257666632c89b71e64ae
Matthew Packham <matthew_packham@outlook.com>
2023-04-05 10:10:21 +0100

    Adding PUT and DEL requests + Basic Error handling

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import stores, items
 
 #create Flask app (application) --> flask run
@@ -20,6 +21,15 @@ def create_store():
     """POST a store to our stores dict"""
     #first we need to get the JSON the client has sent us - request is a module from the flask package
     store_data = request.get_json()
+    
+    ### ERROR HANDLEING ###
+    if 'name' not in store_data:
+        abort(404,
+              message="Bad request. Ensure 'name' is inculded in JSON payload.")
+    ### CHECK store doesnt already exist!
+    for store in stores.items():
+        if store_data['name'] == store['name']:
+            abort(404, message='Store already exists!')
     #create a unique_id 
     store_id = uuid.uuid4().hex
     #create new object to append to stores list
@@ -32,8 +42,27 @@ def create_store():
 def create_item():
     #grab incoming data
     item_data = request.get_json()
+    
+    ### ERROR HANDLING - will check types with marshmallow later ###
+    if (
+        "price" not in item_data or
+        "store_id" not in item_data or
+        "name" not in item_data
+    ):
+        abort(404,
+              message="Bad Request. Ensure 'price', 'store_id' and 'name' are present in payload.")
+    
+    #### CHECK that item doesnt already exist
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"] and
+            item_data["store_id"] == item["store_id"]
+        ):
+            abort(404,
+                  message='Item already exists within that store.')
+    
     if item_data['store_id'] not in stores: #check if we have a matching key in our stores dict
-        return {'message': 'store not found'}, 404
+        abort(404, message='Store not found.') #abort will exit function so no need to return! also helps with documentation
     else:
         # assuming all details are correct
         item_id = uuid.uuid4().hex
@@ -52,7 +81,7 @@ def get_store_name(store_id):
     try:
         return stores[store_id]
     except KeyError as e:
-        return {"message": "Store name not found!"}, 404
+        abort(404, message="Store name not found.")
 
 #get A specific stores items - based on /<string:name> endpt
 @app.get("/item/<string:item_id>")
@@ -60,4 +89,38 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError as e:
-        return {'message': 'item not found'}, 404
+        abort(404, message="Item not found.")
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {'message': 'Item deleted'}, 201
+    except KeyError as e:
+        abort(404, message="Item not found.")
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {'message': 'Store deleted'}, 201
+    except KeyError as e:
+        abort(404, message="store not found.")
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+
+    ### ERROR HANDLEING ###
+    if "name" not in item_data or "price" not in item_data:
+        abort(404,
+              message="Bad Request. Ensure 'name' and 'price' are in JSON Payload.")
+
+    try:
+        item = items[item_id]
+        print('here')
+        item |= item_data #new dictionary update inplace method |= it merges the two dict - if keys exist they are replaced!
+        return item
+    except KeyError as e:
+        abort(404,
+              message="Item not found.")
